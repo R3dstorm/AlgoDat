582a975b2abb8d6d8166d5d131408ad30febf691
Unknown <warri1@gmx.net>
2017-10-31 16:24:40 +0100

    Added File to evaluate runtime for heap sort with log in "performance_results"

--- heap_sort/evaluate_heap_sort.py
+++ heap_sort/evaluate_heap_sort.py
@@ -1,18 +1,76 @@
 #! /usr/bin/python3
 
 
+import random
+import time
 import math
+from heapSort import heap_sort
 
+import numpy as np
+import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+#
+# X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+# C, S = np.cos(X), np.sin(X)
+#
+# plt.plot(X, C)
+# plt.plot(X, S)
+#
+# plt.show()
+
+
+def plot_runtime ():
+    """
+
+    :return:
+    """
 
 # Generate Heapsort runtime plot T(n) with different input sizes n as shown in lecture "minsort".
 # Choose appropriate sizes to get a meaningful plot with enough data points and reasonable runtime (1 minute).
 
-# Generate arrays with big data sizes...
 
-# Start time measurement
+    X = [0]
+    run_time = [0]
+    PERF_OUT = open ("performance_results.txt", "w")
 
-# Start sorting
+    # Generate arrays with big data sizes...
+    for i in range(8):
+        n = int(math.pow(10,i))
+        data = generate_Data(n)
 
-# Stop time measurement
+        # Start time measurement
+        start_time = time.clock()
 
-# Plot running time
+        # Start sorting
+        data = heap_sort(data)
+
+        # Stop time measurement
+        stop_time = time.clock()
+
+        # Plot running time
+        X.append(n)
+        runtime_in_seconds = stop_time - start_time
+        run_time.append(runtime_in_seconds)
+        PERF_OUT.write("%d\t%.1f\n" % (n, runtime_in_seconds))
+
+    PERF_OUT.close()
+    plt.plot(X, run_time)
+    plt.show()
+    return 0
+
+
+def generate_Data (number_data):
+
+    array = [1]
+    for i in range (number_data):
+        array.append(int(random.random()*10000))
+
+    return array
+
+
+if __name__ == "__main__":
+    # Create an unsorted list of integers.
+    # numbers = [10, 4, 1, 5, 2, 3, 11, 3, 9, 12 ,15 ,17, 11, 166, 16, 2]
+    # Sort the list.
+    plot_runtime()
+    #print(generate_Data(10))
